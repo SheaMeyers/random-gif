224a2bb64fd5c2f4335f61a16e1b719d0275dd52
Shea Meyers <shea.meyers@gmail.com>
2021-12-10 19:26:20 +0100

    Set up gif refreshing

--- random-gif/src/App.tsx
+++ random-gif/src/App.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useEffect } from 'react';
+import axios from 'axios';
+import { giphyApiKey } from './keys';
 import './App.css';
 
+
+const initialState = {
+  src: '',
+  title: '',
+  rating: '',
+};
+
+const reducer = (_state: any, action: any) => {
+  return {
+    src: action.embed_url,
+    title: action.title,
+    rating: action.rating
+  }
+}
+
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const newGifInterval = 10000; // 10 seconds
+
+  const getNewGif = () => {
+    axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${giphyApiKey}`)
+      .then(response => dispatch(response.data.data))
+      .catch(error => console.log(error));
+  }
+
+  useEffect(() => {
+    getNewGif();
+    const interval = setInterval(getNewGif, newGifInterval);
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <div className="App">
-      <iframe src="https://giphy.com/embed/IdZL90nzpBn362PNN0" title="random-gif" width="480" height="270" frameBorder="0"/>
+      <iframe src={state.src} title="random-gif" width="480" height="270" frameBorder="0"/>
       <div className="Gif-Info">
         <div>
           <p><b>Title</b></p>
-          <p>GIF by NBA</p>
+          <p>{state.title}</p>
         </div>
         <div>
           <p><b>Rating</b></p>
-          <p>{'g'.toUpperCase()}</p>
+          <p>{state.rating.toUpperCase()}</p>
         </div>
       </div>
     </div>
---
+++ random-gif/src/keys.ts
@@ -0,0 +1,5 @@
+// Note: In a real world scenario this should be retrieved from the environment
+//   using process.env or something similar so the Api Key is not publicly exposed
+const giphyApiKey = 'dc6zaTOxFJmzC';
+
+export { giphyApiKey } ;
