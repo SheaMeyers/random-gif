ff55cf84a6488b6cb87250ba8a5e6d8d521f550d
Shea Meyers <shea.meyers@gmail.com>
2021-12-10 19:44:50 +0100

    Display search bar in page

--- random-gif/src/App.tsx
+++ random-gif/src/App.tsx
@@ -1,26 +1,27 @@
-import React, { useReducer, useEffect } from 'react';
-import axios from 'axios';
-import { giphyApiKey } from './keys';
-import './App.css';
-
+import React, { useReducer, useEffect } from "react";
+import axios from "axios";
+import Icon from "@mui/material/Icon";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import { giphyApiKey } from "./keys";
+import "./App.css";
 
 const initialState = {
-  src: '',
-  title: '',
-  rating: '',
+  src: "",
+  title: "",
+  rating: "",
 };
 
 const reducer = (_state: any, action: any) => {
   return {
     src: action.embed_url,
     title: action.title,
-    rating: action.rating
-  }
-}
-
+    rating: action.rating,
+  };
+};
 
 function App() {
-
   const [state, dispatch] = useReducer(reducer, initialState);
   const newGifInterval = 10000; // 10 seconds
 
@@ -28,24 +29,48 @@ function App() {
     axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${giphyApiKey}`)
       .then(response => dispatch(response.data.data))
       .catch(error => console.log(error));
-  }
+  };
 
   useEffect(() => {
     getNewGif();
     const interval = setInterval(getNewGif, newGifInterval);
     return () => clearInterval(interval);
   }, []);
-  
+
   return (
     <div className="App">
-      <iframe src={state.src} title="random-gif" width="480" height="270" frameBorder="0"/>
+      <TextField
+        label="Search"
+        variant="outlined"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <Icon>
+                <SearchIcon />
+              </Icon>
+            </InputAdornment>
+          ),
+        }}
+        onChange={(event) => console.log(event.target.value)}
+      />
+      <iframe
+        src={state.src}
+        title="random-gif"
+        width="480"
+        height="270"
+        frameBorder="0"
+      />
       <div className="Gif-Info">
         <div>
-          <p><b>Title</b></p>
+          <p>
+            <b>Title</b>
+          </p>
           <p>{state.title}</p>
         </div>
         <div>
-          <p><b>Rating</b></p>
+          <p>
+            <b>Rating</b>
+          </p>
           <p>{state.rating.toUpperCase()}</p>
         </div>
       </div>
