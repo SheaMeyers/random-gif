c72adf17891e217384d4e9a8d7d25378c00c6b30
Shea Meyers <shea.meyers@gmail.com>
2021-12-10 18:38:23 +0100

    Display first gif

--- random-gif/src/App.tsx
+++ random-gif/src/App.tsx
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe src="https://giphy.com/embed/IdZL90nzpBn362PNN0" title="random-gif" width="480" height="270" frameBorder="0"/>
     </div>
   );
 }
