b2785f41f873851696f8098d02787400b325e8aa
Shea Meyers <shea.meyers@gmail.com>
2021-12-10 18:54:38 +0100

    Add basic layout for title and rating of gif

--- random-gif/src/App.tsx
+++ random-gif/src/App.tsx
@@ -6,6 +6,16 @@ function App() {
   return (
     <div className="App">
       <iframe src="https://giphy.com/embed/IdZL90nzpBn362PNN0" title="random-gif" width="480" height="270" frameBorder="0"/>
+      <div className="Gif-Info">
+        <div>
+          <p><b>Title</b></p>
+          <p>GIF by NBA</p>
+        </div>
+        <div>
+          <p><b>Rating</b></p>
+          <p>{'g'.toUpperCase()}</p>
+        </div>
+      </div>
     </div>
   );
 }
