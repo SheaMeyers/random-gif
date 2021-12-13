18ee5953f77e1da1d4814cc75f9aee3bb9e6b3df
Shea Meyers <shea.meyers@gmail.com>
2021-12-13 20:08:01 +0100

    Update

--- random-gif/src/keys.ts
+++ random-gif/src/keys.ts
@@ -1,5 +1,5 @@
 // Note: In a real world scenario this should be retrieved from the environment
 //   using process.env or something similar so the Api Key is not publicly exposed
-const giphyApiKey = 'dc6zaTOxFJmzC';
+const giphyApiKey = 'N6Hk8HXId49DwxEzUEBc4YzpjL97eZ1D';
 
 export { giphyApiKey } ;
