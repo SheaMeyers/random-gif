8346d0cdd1e8013ea9b7fd60330b7d4cb9972f8c
Shea Meyers <shea.meyers@gmail.com>
2021-12-26 21:54:22 +0100

    Fix mobile styling

--- src/components/DisplayGif.tsx
+++ src/components/DisplayGif.tsx
@@ -9,28 +9,27 @@ interface Props {
 
 const DisplayGif = (props: Props) => {
     return (
-        <>
-          <iframe
-            src={props.src}
-            title="random-gif"
-            width="480"
-            height="270"
-            frameBorder="0"
-          />
-          <br/><br/>
-          <div className="Gif-Info">
-            <div className="Gif-Info__Container">
-              <div className="Gif-Info__Title-and-Rating">
-                <p className="Gif-Info__Title">{props.title}</p>
-                <a href={props.url}>{props.url}</a>
-              </div>
-              <div className="Gif-Info__Rating">
-                <b>{props.rating.toUpperCase()}</b>
-              </div>
+      <>
+        <iframe 
+          src={props.src} 
+          className="Gif" 
+          title="random-gif" 
+          frameBorder="0" 
+        />
+        <br /><br />
+        <div className="Gif-Info">
+          <div className="Gif-Info__Container">
+            <div className="Gif-Info__Title-and-Rating">
+              <p className="Gif-Info__Title">{props.title}</p>
+              <a href={props.url}>{props.url}</a>
+            </div>
+            <div className="Gif-Info__Rating">
+              <b>{props.rating.toUpperCase()}</b>
             </div>
           </div>
-        </>
-    )
+        </div>
+      </>
+    );
 }
 
 export default DisplayGif;
